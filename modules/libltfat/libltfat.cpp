527bfb2b7f56d475fb0c423a17060931df485a50
Zdenek Prusa <zdenek.prusa@gmail.com>
2017-07-19 19:25:24 +0200

    Moved libltfat module

---
+++ modules/libltfat/libltfat.cpp
@@ -0,0 +1,47 @@
+#include "libltfat.h"
+
+#ifdef DEBUG
+#undef DEBUG
+#endif
+
+#ifdef LTFAT_SINGLE
+#undef LTFAT_SINGLE
+#endif
+
+#ifdef LTFAT_DOUBLE
+#undef LTFAT_DOUBLE
+#endif
+
+#ifdef LTFAT_COMPLEXTYPE
+#undef LTFAT_COMPLEXTYPE
+#endif
+
+#define LTFAT_SINGLE
+#include "libltfat/src/rtdgtreal.c"
+#include "libltfat/src/ci_utils.c"
+#include "libltfat/src/utils.c"
+#if USEFFTW
+#include "libltfat/src/fftw_wrappers.c"
+#else
+#include "libltfat/thirdparty/kissfft/fft.c"
+#include "libltfat/thirdparty/kissfft/fftr.c"
+#include "libltfat/src/kissfft_wrappers.c"
+#endif
+#include "libltfat/src/ci_memalloc.c"
+#include "libltfat/src/heap.c"
+#include "libltfat/src/gabdual_painless.c"
+#include "libltfat/src/ci_windows.c"
+#include "libpv/pv.c"
+#include "libpv/rtpghi.c"
+
+#include "libltfat/src/memalloc.c"
+#define LTFAT_COMPLEXTYPE
+#include "libltfat/src/ci_memalloc.c"
+#undef LTFAT_COMPLEXTYPE
+
+#undef LTFAT_SINGLE
+
+
+#include "libltfat/src/error.c"
+#include "libltfat/src/integer_manip.c"
+
---
+++ modules/libltfat/libltfat.h
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ The block below describes the properties of this module, and is read by
+ the Projucer to automatically generate project code that uses it.
+ For details about the syntax and how to create or use a module, see the
+ JUCE Module Format.txt file.
+
+
+ BEGIN_JUCE_MODULE_DECLARATION
+
+  ID:               libltfat
+  vendor:           Zdenek Prusa, Peter L. Soendergaard
+  version:          0.0.2
+  name:             libltfat
+  description:      The Large Time Frequency Analysis Toolbox Library
+  website:          http://ltfat.github.io/libltfat
+  license:          GPL
+
+  searchpaths:      libltfat/thirdparty libltfat/include
+    linuxLibs:      fftw3f
+
+ END_JUCE_MODULE_DECLARATION
+
+*******************************************************************************/
+
+//==============================================================================
+/** Config: NOBLASLAPACK
+
+*/
+#ifndef USEBLASLAPACK
+#define USEBLASLAPACK 0
+#endif
+
+//==============================================================================
+/** Config: USEFFTW
+
+*/
+#ifndef USEFFTW
+#define USEFFTW 0
+#endif
+
+#if USEFFTW
+#define FFTW 1
+#else
+#define KISS 1
+#endif
+
+#define LTFAT_SINGLE 1
+#define LTFAT_COMPAT32 1
+#define LTFAT_API
+
+#include "libltfat/include/ltfat.h"
+#include "libpv/pv.h"
